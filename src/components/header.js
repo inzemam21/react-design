45c51b736d10a4e8b464b17fe0fd3a54e2330065
inzimam Ul Haq <inzemamsmile2@gmail.com>
2021-12-14 22:55:27 +0500

    implemented header and adaptive layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,20 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from '../images/logo-designcode.svg'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+  <div className='Header'>
+      <div className='HeaderGroup'>
+         <Link to='/'><img src={logo} width='30'/></Link>
+          <Link to='/Courses'>Courses</Link>
+          <Link to='/Downloads'>Downloads</Link>
+          <Link to='/WorkShops'>WorkShops</Link>
+          <Link to='/Buy'><button>Buy</button></Link>
+      </div>
+  </div>
   </header>
 )
 
