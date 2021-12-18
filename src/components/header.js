93a92f6dbee145d89ae401f4861089a03c29462b
inzimam Ul Haq <inzemamsmile2@gmail.com>
2021-12-18 23:24:18 +0500

    Stateful header

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,49 @@
-import * as React from "react"
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import logo from '../images/logo-designcode.svg'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <header>
-  <div className='Header'>
-      <div className='HeaderGroup'>
-         <Link to='/'><img src={logo} width='30'/></Link>
-          <Link to='/Courses'>Courses</Link>
-          <Link to='/Downloads'>Downloads</Link>
-          <Link to='/WorkShops'>WorkShops</Link>
-          <Link to='/Buy'><button>Buy</button></Link>
-      </div>
-  </div>
-  </header>
-)
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            hasScrolled: false
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = (event) => {
+        const scrollTop = window.pageYOffset
+
+        if (scrollTop > 50) {
+            this.setState({ hasScrolled: true })
+        } else {
+            this.setState({ hasScrolled: false })
+        }
+    }
+
+    render() {
+        return (
+            <header>
+            <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+                <div className="HeaderGroup">
+                    <Link to="/"><img src={require('../images/logo-designcode.svg').default} width="30" /></Link>
+                    <Link to="/courses">Courses</Link>
+                    <Link to="/downloads">Downloads</Link>
+                    <Link to="/workshops">Workshops</Link>
+                    <Link to="/buy"><button>Buy</button></Link>
+                </div>
+            </div>
+            </header>
+
+        )
+    }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
