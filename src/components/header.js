1f37a1e6025d3d211c820701a850b38de9a3b3ad
inzimam Ul Haq <inzemamsmile2@gmail.com>
2021-12-19 22:38:42 +0500

    styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import logo from '../images/logo-designcode.svg'
 import './Header.css'
 
 class Header extends React.Component {
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,57 +11,54 @@ import logo6 from '../images/logo-swift.png'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Card from "../components/Card";
+import Wave from "../components/Wave";
+import Section from "../components/Section";
 
 const IndexPage = () => (
   <Layout>
-      <div className='Hero'>
-          <div className='HeroGroup'>
-              <Seo title="Home" />
-              <h1>Learn To <br /> Design And Code React apps</h1>
-              <p>bobob guyfyc op[oiu tdxfx</p>
-              <Link to="/page-2/">Go to page 2</Link>
+      <div className="Hero">
+          <div className="HeroGroup">
+              <h1>Learn to <br />design and code React apps</h1>
+              <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+              <Link to="/page-2/">Watch the video</Link>
               <div className="Logos">
-                  <img src={logo1} width="50" />
-                  <img src={logo2} width="50" />
-                  <img src={logo3} width="50" />
-                  <img src={logo4} width="50" />
-                  <img src={logo5} width="50" />
-                  <img src={logo6} width="50" />
+                  <img src={require('../images/logo-sketch.png').default} width="50" />
+                  <img src={require('../images/logo-figma.png').default} width="50" />
+                  <img src={require('../images/logo-studio.png').default} width="50" />
+                  <img src={require('../images/logo-framer.png').default} width="50" />
+                  <img src={require('../images/logo-react.png').default} width="50" />
+                  <img src={require('../images/logo-swift.png').default} width="50" />
               </div>
-              <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill="white">
-                      <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s" values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-            M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
-            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-
-            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
-            "
-                      />
-                  </path>
-              </svg>
-          </div>
-          <div className='Cards'>
-              <h2>11 courses more coming</h2>
-              <div className="CardGroup">
-                  <Card
-                      title="Design System"
-                      text="10 sections"
-                      image={require('../images/wallpaper.jpg').default} />
-                  <Card
-                      title="React for Designers"
-                      text="12 sections"
-                      image={require('../images/wallpaper2.jpg').default} />
-                  <Card
-                      title="Sound Design"
-                      text="5 sections"
-                      image={require('../images/wallpaper3.jpg').default} />
-                  <Card
-                      title="ARKit 2"
-                      text="10 sections"
-                      image={require('../images/wallpaper4.jpg').default} />
-              </div>
+              <Wave />
           </div>
       </div>
+      <div className="Cards">
+          <h2>11 courses, more coming</h2>
+          <div className="CardGroup">
+              <Card
+                  title="Design System"
+                  text="10 sections"
+                  image={require('../images/wallpaper.jpg').default} />
+              <Card
+                  title="React for Designers"
+                  text="12 sections"
+                  image={require('../images/wallpaper2.jpg').default} />
+              <Card
+                  title="Sound Design"
+                  text="5 sections"
+                  image={require('../images/wallpaper3.jpg').default} />
+              <Card
+                  title="ARKit 2"
+                  text="10 sections"
+                  image={require('../images/wallpaper4.jpg').default} />
+          </div>
+      </div>
+      <Section
+          image={require('../images/wallpaper2.jpg').default}
+          logo={require('../images/logo-react.png').default}
+          title="React for Designers"
+          text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+      />
   </Layout>
 )
 
